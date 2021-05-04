0210dd7a7a7a6d2d92ec94bf1a1b5171d25d58cc
MaciejPlonowski98 <76108245+MaciejPlonowski98@users.noreply.github.com>
2021-05-04 20:41:48 +0200

    Add files via upload
    added calorie calculation

--- scripts/mymodal.js
+++ scripts/mymodal.js
@@ -1,7 +1,7 @@
 // Get the modal
 var modal = document.getElementById("myModal");
 var modal1 = document.getElementById("myModal1");
-        
+
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 var btn1 = document.getElementById("myBtn1");
@@ -16,33 +16,34 @@ var button1 = document.getElementsByClassName("dodaj1")[0];
 
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
 }
 
-btn1.onclick = function() {
+btn1.onclick = function () {
   modal1.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
-span1.onclick = function() {
+span1.onclick = function () {
   modal1.style.display = "none";
 }
 
-button.onclick = function() {
-    updateSettings()
+button.onclick = function () {
+  updateSettings()
+  calculateCPM()
 }
 
-button1.onclick = function() {
-    addMeasurement()    
+button1.onclick = function () {
+  addMeasurement()
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal || event.target == modal1) {
     modal.style.display = "none";
     modal1.style.display = "none";
